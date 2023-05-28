c5476966623fcf82690be7db44983b89bfbd2ae8
Tomi Malamud <tomasmalamud@gmail.com>
2023-05-27 23:36:42 -0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
-
 module.exports = {
 	content: ["encuestas/templates/**/*.html"],
 	theme: {
 		extend: {},
 	},
-	plugins: [require('@tailwindcss/forms')],
+	plugins: [],
 };
