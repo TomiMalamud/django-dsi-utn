6d89828c47d231892c0231965255d88d0f7f920e
Tomi Malamud <tomasmalamud@gmail.com>
2023-05-28 00:07:52 -0300

    ultimo intento

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,7 +34,6 @@ INTERNAL_IPS = [
 INSTALLED_APPS = [
     'tailwind',
     'theme',
-    'theme2',
     'django_browser_reload',
     'encuestas.apps.EncuestasConfig',
     'django.contrib.admin',
@@ -128,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'theme/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
